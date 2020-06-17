f9399f1d03b04cac5b0897d601d110af9e7549ff
MethodFunc <tjfgusfkd@gmail.com>
2020-06-17 16:34:48 +0900

    20200617 수요일

--- Titanic.py
+++ Titanic.py
@@ -3,7 +3,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.preprocessing import LabelEncoder
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, KFold, cross_val_score, GridSearchCV
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
 %matplotlib inline
 
 titanic_df = pd.read_csv('titanic_train.csv')
@@ -71,7 +75,7 @@ group_names = ['Unknown', 'Baby', 'Child', 'Teenager',
 
 # lambda 식에 위에서 생성한 get_category() 함수를 반환값으로 지정
 # get_category(x) 는 입력값으로 'Age' 칼럼 값을 받아서 해당되는 Cat 반환
-titanic_df['Age_cat'] = titanic_df['Age'].apply(lambda x : get_category(x))
+titanic_df['Age_cat'] = titanic_df['Age'].apply(lambda x: get_category(x))
 sns.barplot('Age_cat', 'Survived', 'Sex', titanic_df, group_names)
 titanic_df.drop('Age_cat', axis=1, inplace=True)
 
@@ -79,30 +83,37 @@ titanic_df.drop('Age_cat', axis=1, inplace=True)
 def encode_features(dataDF):
     Features = ['Cabin', 'Sex', 'Embarked']
     for feature in Features:
-        le = preprocessing.LabelEncoder()
+        le = LabelEncoder()
         le = le.fit(dataDF[feature])
         dataDF[feature] = le.transform(dataDF[feature])
 
     return dataDF
 
+
 titanic_df = encode_features(titanic_df)
 titanic_df.head()
 
 # Null 처리 함수
+
+
 def fillna(df):
     df['Age'].fillna(df['Age'].mean(), inplace=True)
     df['Cabin'].fillna('N', inplace=True)
     df['Embarked'].fillna('N', inplace=True)
     df['Fare'].fillna(0, inplace=True)
-    
+
     return df
 
 # 머신러닝 알고리즘에 불필요한 속성 제거
+
+
 def drop_features(df):
     df.drop(['PassengerId', 'Name', 'Ticket'], axis=1, inplace=True)
     return df
 
 # label encoding
+
+
 def format_features(df):
     df['Cabin'] = df['Cabin'].str[:1]
     features = ['Cabin', 'Sex', 'Embarked']
@@ -131,3 +142,95 @@ x_titanic_df = titanic_df.drop('Survived', axis=1)
 x_titanic_df = transform_features(x_titanic_df)
 
 x_train, x_test, y_train, y_test = train_test_split(x_titanic_df, y_titanic_df, test_size=0.2, random_state=11)
+
+#결정 트리, Random Forest, 로지스틱 희귀를 위한 사이킷런 classifier 클래스 생성
+dt_clf = DecisionTreeClassifier(random_state=11)
+rf_clf = RandomForestClassifier(random_state=11)
+lr_clf = LogisticRegression()
+
+#DecisionTreeClassfier 학습/예측/평가
+dt_clf.fit(x_train, y_train)
+dt_pred = dt_clf.predict(x_test)
+print('DecisionTreeClassifier 정확도:{0:.4f}'.format(accuracy_score(y_test, dt_pred)))
+
+# RandomForestClassifier 학습/예측/평가
+rf_clf.fit(x_train, y_train)
+rf_pred = rf_clf.predict(x_test)
+print('RandomForestClassifier 정확도:{0:.4f}'.format(accuracy_score(y_test, rf_pred)))
+
+#LogisticRegression 학습/예측/평가
+lr_clf.fit(x_train, y_train)
+lr_pred = lr_clf.predict(x_test)
+print('LogisticRegression 정확도:{0:.4f}'.format(accuracy_score(y_test, lr_pred)))
+
+
+def exec_kfold(clf, folds=5):
+    # Fold를 5개생성
+    kfold = KFold(n_splits=folds)
+    scores = []
+
+    # KFold 교차검증 진행
+    for iter_count, (train_index, test_index) in enumerate(kfold.split(x_titanic_df)):
+        x_train, x_test = x_titanic_df.values[train_index],  x_titanic_df.values[test_index]
+        y_train, y_test = y_titanic_df[train_index], y_titanic_df[test_index]
+        # classifier 학습 예측 정확도 계산
+        clf.fit(x_train, y_train)
+        predictions = clf.predict(x_test)
+        accuracy = accuracy_score(y_test, predictions)
+        scores.append(accuracy)
+        print('교차 검증 {0} 정확도: {1:.4f}'.format(iter_count, accuracy))
+
+    mean_score = np.mean(scores)
+    print("평균 정확도 : {0:.4f}".format(mean_score))
+
+
+# exec_kfold 호출
+exec_kfold(dt_clf, folds=5)
+
+# Cross_val_Score
+scores = cross_val_score(dt_clf, x_titanic_df, y_titanic_df, cv=5)
+for iter_count, accuracy in enumerate(scores):
+    print('교차 검증 {0} 정확도: {1:.4f}'.format(iter_count, accuracy))
+
+print("평균 정확도 : {0:.4f}".format(np.mean(scores)))
+
+# GridSearchCV
+parameters = {'max_depth': [2, 3, 5, 10], 'min_samples_split': [2, 3, 5], 'min_samples_leaf': [1, 5, 8]}
+
+grid_dclf = GridSearchCV(dt_clf, param_grid=parameters, scoring='accuracy', cv=5)
+grid_dclf.fit(x_train, y_train)
+
+print('GridSearchCV 최적 하이퍼 파라미터 : ', grid_dclf.best_params_)
+print('GridSearchCV 최적 정확도 :{0:.4f}'.format(grid_dclf.best_score_))
+
+best_dclf = grid_dclf.best_estimator_
+
+# GridSearchCV의 최적 하이퍼 파라미터로 학습된 Estimator로 예측 및 평가 수핵
+dpredictions = best_dclf.predict(x_test)
+accuracy = accuracy_score(y_test, dpredictions)
+print('테스트 세트에서의 DecisionTreeClassifier 정확도 : {0:.4f}'.format(accuracy))
+
+class MyDummyClassfifier(BaseEstimator):
+    def fit(self, x, y=None):
+        pass
+    def predict(self, x):
+        pred =np.zeros((x.shape[0], 1))
+        for i in range(x.shape[0]):
+            if x['Sex'].iloc[i] ==1:
+                pred[i] = 0
+            else:
+                pred[i] = 1
+
+        return pred        
+
+titanic_df = pd.read_csv('titanic_train.csv')
+y_titanic_df = titanic_df['Survived']
+x_titanic_df = titanic_df.drop('Survived', axis=1)
+x_titanic_df = transform_features(x_titanic_df)
+x_train, x_test, y_train, y_test = train_test_split(x_titanic_df, y_titanic_df, test_size=0.2, random_state=11)
+
+myclf = MyDummyClassfifier()
+myclf.fit(x_train, y_train)
+
+mypredctions = myclf.predict(x_test)
+print('Dummy Classifier의 정확도는: {0:.4f}'.format(accuracy_score(y_test, mypredctions)))
